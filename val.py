15659e6a911783c996d0503801c21170c6741d30
Chou_isme <38151444-ImChouOWO@users.noreply.gitlab.com>
2026-07-20 13:48:11 +0800

    Add Val scr

--- val.py
+++ val.py
@@ -3,5 +3,6 @@ import sys
 import os
 from datetime import datetime
 from model.utils.inferScr.infer import val
+#val script for validation of the model, it will generate a log file in the specified path with the results of the validation.
 if __name__ == "__main__":
     val(path="model/res/val")
